a1946709861d225478b83c7c9bcf9d3b19efe232
David <david.santos54@fatec.sp.gov.br>
2022-11-18 21:53:54 -0300

    Cadastrar Solicitante

--- src/pages/solicitante/cadastrarsolicitante/CadastrarSolicitante.jsx
+++ src/pages/solicitante/cadastrarsolicitante/CadastrarSolicitante.jsx
@@ -1,12 +1,98 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import './cadastrarsolicitante.css'
+import { useNavigate } from 'react-router-dom'
 
 const CadastrarSolicitante = () => {
-  return (
-    <div className='cadastrarsolicitante__container'>
-        Cadastrar
-    </div>
-  )
+    let navigate = useNavigate()
+
+    const [solicitante, setSolicitante] = useState({
+        cnpj: '',
+        nomeSolicitante: '',
+        cep: '',
+        endereco: '',
+        cidade: '',
+        estado: '',
+        emailComercial: '',
+        telefoneComercial: ''
+    })
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        setErrorModal(true)
+        await axios.post('http://localhost:8080/solicitante', solicitante)
+        navigate('/consultar-solicitantes')
+    }
+
+    const onInputChange = (e) => {
+        setSolicitante({ ...solicitante, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div className='cadastrarsolicitante__container'>
+            <h2>Cadastrar Solicitante</h2>
+            <h3>Informe abaixo os dados do solicitante que será cadastrado</h3>
+            <form onSubmit={(e) => onSubmit(e)} className="cadastrarsolicitante__form">
+                <div className="cadastrarsolicitante__form-input">
+                    <label htmlFor="cnpj">
+                        CNPJ
+                    </label>
+                    <input className='left' type="text" placeholder='Informe o cnpj do solicitante' name='cnpj' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label className='right' htmlFor="nomeSolicitante">
+                        Nome Fantasia
+                    </label>
+                    <input className='right' type="text" placeholder='Informe o nome fantasia do solicitante' name='nomeSolicitante' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label htmlFor="cep">
+                        CEP
+                    </label>
+                    <input className='left' type="text" placeholder='Informe o cep do solicitante' name='cep' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label className='right' htmlFor="endereco">
+                        Endereço
+                    </label>
+                    <input className='right' type="text" placeholder='Informe o endereço do solicitante' name='endereco' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label htmlFor="cidade">
+                        Cidade
+                    </label>
+                    <input className='left' type="text" placeholder='Informe a cidade do solicitante' name='cidade' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label className='right' htmlFor="estado">
+                        Estado
+                    </label>
+                    <input className='right' type="text" placeholder='Informe o estado do solicitante' name='estado' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label htmlFor="emailComercial">
+                        Email Comercial
+                    </label>
+                    <input className='left' type="text" placeholder='Informe o email comercial do solicitante' name='emailComercial' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <div className="cadastrarsolicitante__form-input">
+                    <label className='right' htmlFor="telefoneComercial">
+                        Telefone Comercial
+                    </label>
+                    <input className='right' type="text" placeholder='Informe o cnpj do solicitante' name='telefoneComercial' onChange={(e) => onInputChange(e)} required />
+                </div>
+
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+    )
 }
 
 export default CadastrarSolicitante
